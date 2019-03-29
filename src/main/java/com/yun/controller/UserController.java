594fe374106315d83cfffd92a5d7b7c7a031966d
Lesoleil <2012289350@qq.com>
2019-03-29 12:44:52 +0800

    yunmusic项目创建

---
+++ src/main/java/com/yun/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.yun.controller;
+
+import com.yun.service.RegAndLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+    //@Autowired是依赖注入，注入service层
+    @Autowired
+    private RegAndLoginService regAndLoginService;
+
+    //注册账号
+    @RequestMapping("/reg")
+    public String reg(){return "index";}
+
+}
